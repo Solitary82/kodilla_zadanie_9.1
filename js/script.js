51a1960e5db512cf6b5140ea9f537d69ec7cd737
Marta Waplak <mwaplak@gmail.com>
2018-02-02 23:33:16 +0100

    Modify .js file

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 function getTriangleArea(a, h) {
-    
+   
     "use strict";
     
-    if (a && h >= 0) {
+    if (a  >= 0 && h >= 0) {
         return(a*h/2);
     } else {
         return("Nieprawidłowa wartość");
     }
+
 }
+    
+    console.log(getTriangleArea(10, 6));
 
-console.log(getTriangleArea(10, 6));
+    var triangle1Area = getTriangleArea(10, 15);
+    console.log(triangle1Area);
 
-var triangle1Area=getTriangleArea(10, 15);
-console.log(triangle1Area);
+    var triange2Area = getTriangleArea(-2, 8);
+    console.log(triange2Area);
 
-var triangle2Area=getTriangleArea(20, 30);
-console.log(triangle2Area);
-
-var triangle3Area=getTriangleArea(5, 8);
-console.log(triangle3Area);
+    var triangle3Area = getTriangleArea(6, -30);
+    console.log(triangle3Area);
